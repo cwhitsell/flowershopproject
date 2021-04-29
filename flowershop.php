13aedb2ad101b2e29b4dac1a2f1158045186347a
cwhitsell <49255787+cwhitsell@users.noreply.github.com>
2021-04-28 20:55:05 -0700

    Update flowershop.php

--- flowershop.php
+++ flowershop.php
@@ -1,5 +1,7 @@
 <?php
+    echo "Hellow World2";
 
+/*
 //dbinfo
 $servername = "flowershop1.c1afi6dhlbym.us-west-1.rds.amazonaws.com";
 $rootuser = "admin";
@@ -38,6 +40,7 @@ $query = "SELECT * FROM CUSTOMER WHERE PhoneNumber='$phonenumber'";
         }
 
 mysqli_close($db);
+*/
 
 
 ?>
